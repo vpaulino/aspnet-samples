5ffe15410dd14763fe32ccf56a53b6fecf820982
Vitor.Paulino <Vitor.Paulino@vision-box.com>
2018-05-23 18:04:22 +0100

    base code to sample added

---
+++ MessagePackWebApi/Implementations/InMemoryProcessProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Models.Processes;
+
+namespace MessagePackWebApi.Implementations
+{
+    public class InMemoryProcessProvider : IProcessProvider
+    {
+        Dictionary<int, Process> processes = new Dictionary<int, Process>();
+
+        public Task<bool> Create(Process process)
+        {
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+           var result = processes.TryAdd(process.Id, process);
+
+            tcs.SetResult(result);
+
+            return tcs.Task;
+        }
+
+        public Task<bool> Delete(int id)
+        {
+
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+            var result = processes.Remove(id);
+
+            tcs.SetResult(result);
+
+            return tcs.Task;
+        }
+
+        public Task<Process> Get(int id)
+        {
+            TaskCompletionSource<Process> tcs = new TaskCompletionSource<Process>();
+            Process process;
+
+            var result = processes.TryGetValue(id, out process);
+
+            tcs.SetResult(process);
+
+            return tcs.Task;
+        }
+
+        public Task<IEnumerable<Process>> Get(Func<Process, bool> predicate, int take, int skip)
+        {
+            TaskCompletionSource<IEnumerable<Process>> tcs = new TaskCompletionSource<IEnumerable<Process>>();
+
+           var result = this.processes.Values.Where(predicate).Skip(skip).Take(take);
+
+            tcs.SetResult(result);
+
+            return tcs.Task;
+        }
+    }
+}
