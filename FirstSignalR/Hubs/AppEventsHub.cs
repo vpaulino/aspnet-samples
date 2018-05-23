733257392417a55b3a590b50b7f02796fb6a284a
Vitor.Paulino <Vitor.Paulino@vision-box.com>
2018-05-23 17:36:37 +0100

    models separated to their own project to be reused in all samples

--- FirstSignalR/Hubs/AppEventsHub.cs
+++ FirstSignalR/Hubs/AppEventsHub.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FirstSignalR.Messages;
+using Models.Processes;
 using Microsoft.AspNetCore.SignalR;
 
 namespace FirstSignalR
