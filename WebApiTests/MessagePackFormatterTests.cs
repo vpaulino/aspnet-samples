977f04311d9186b2ca4a30c7b22cc5ff0c2b22e1
Vitor.Paulino <Vitor.Paulino@vision-box.com>
2018-06-01 18:57:40 +0100

    removed project that was not needed

---
+++ WebApiTests/MessagePackFormatterTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiTests
+{
+    public class MessagePackFormatterTests
+    {
+        public void TestProduct_Success()
+        {
+
+        }
+
+    }
+}
