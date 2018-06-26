e698fef7085f4f6cdac724bf0303ac93152102a1
Vitor.Paulino <Vitor.Paulino@vision-box.com>
2018-06-26 18:22:46 +0100

    Exception when overriding ShouldValidateType from DefaultBodyModelValidator type

---
+++ src/Hosts/WebApiHost/ConsoleTraceSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebApiHost
+{
+    public interface IVBTraceSource
+    {
+        bool ShouldTrace(TraceEventType eventType);
+        
+        void TraceEvent(TraceEventType eventType, string id, string message);
+        
+    }
+    public class ConsoleTraceSource : IVBTraceSource
+    {
+        public bool ShouldTrace(TraceEventType eventType)
+        {
+            return true;
+        }
+
+        public void TraceEvent(TraceEventType eventType, string id, string message)
+        {
+            Console.WriteLine($"{eventType} - {id} : {message}");
+        }
+    }
+}
---
+++ src/Hosts/WebApiHost/MediaFormatters/ExtendedJsonMediaFormatter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiHost.MediaFormatters
+{
+    public class ExtendedJsonMediaFormatter : JsonMediaTypeFormatter
+    {
+        public ExtendedJsonMediaFormatter()
+        {
+            this.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("application/json"));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            return true;
+        }
+
+        public override object ReadFromStream(Type type, Stream readStream, Encoding effectiveEncoding, IFormatterLogger formatterLogger)
+        {
+            Stopwatch clock = new Stopwatch();
+            clock.Start();
+            var result= base.ReadFromStream(type, readStream, effectiveEncoding, formatterLogger);
+            clock.Stop();
+            Console.WriteLine($"ExtendedJsonMediaFormatter.End - Default formatter took : {clock.Elapsed}");
+
+            return result;
+
+        }
+    }
+}
